42a3b78cd8cd9c552f780637f1a2b520982fa48f
USER <y.hoshino25@gmail.com>
2022-05-28 01:18:20 +0900

    fix some styles on component detail-page

--- components/SideList.jsx
+++ components/SideList.jsx
@@ -6,25 +6,25 @@ const section = css`
     position: absolute;
     right: 0;
     width: 200px;
-    height: 100vh;
+    height: 90%;
     background-color: rgba(0,0,0, 0.2);
     
     /* margin-left: 100px; */
     box-shadow: 0px 10px 20px rgba(0,0,0, 0.3);
 `
 const header = css`
-    height: 70px;
-    color: #4a567e;
+    height: 60px;
     padding: 10px;
+    color: #4a567e;
 `
 const item_list = css`
     overflow: scroll;
-    height: 85%;
+    height: 80%;
     margin-bottom: 10px;
 `
 const item = css`
     width: 80%;
-    height: 100px;
+    height: 80px;
     background-color: #ffffff86;
     margin: 10px;
     padding: 10px;
@@ -36,7 +36,7 @@ const SideList = ({mdlist}) => {
     return ( 
         <div css={section}>
             <div css={header}>
-                <h2>sideList!</h2>
+                <h2>CompList!</h2>
             </div>
             <div css={item_list}>
             {mdlist.map((md)=>(
